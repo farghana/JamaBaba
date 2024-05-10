10916b1827c0bd378d9e8e4ebc9347194fea08ce
Farghana Hussain <farghana@gmail.com>
2024-05-10 18:53:24 -0400

    Set up basic mysql

---
+++ src/app/api/users/route.js
@@ -0,0 +1,13 @@
+import { query } from "@/lib/db";
+
+export async function GET(request) {
+  const users = await query({
+      query: "SELECT * FROM buyers",
+      values: [],
+  });
+
+  let data = JSON.stringify(users);
+  return new Response(data, {
+      status: 200,
+  });
+}
