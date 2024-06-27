89943448b260dcca54e891f2865709ef8d9fa528
Farghana Hussain <farghana@gmail.com>
2024-06-27 17:05:11 -0400

    Link top nav to correct pages. Show/hide menu on small screen

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,17 +1,20 @@
 "use client";
 
 import { signOut } from "next-auth/react";
+import Link from "next/link";
 
 const Logout = () => {
 	return (
-		<div className='hidden md:block md:ml-6'>
-			<div className='flex items-center'>
-				<button onClick={() => signOut()} className='flex items-center text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'>
-					<i className='fa-brands fa-google text-white mr-2'></i>
-					<span>Logout</span>
-				</button>
-			</div>
-		</div>
+		<Link
+			href='#'
+			className='block px-4 py-2 text-sm text-gray-700'
+			role='menuitem'
+			tabIndex='-1'
+			id='user-menu-item-2'
+			onClick={() => signOut()}
+		>
+			Sign Out
+		</Link>
 	);
 };
 
